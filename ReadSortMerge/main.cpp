c8e2e48da425c7a8303ebfa2a656167991847bea
zmartin96 <zachary1131@gmail.com>
2023-04-10 14:02:55 -0500

    Initial Commit

---
+++ ReadSortMerge/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ReadSortMerge
+//
+//  Created by Zachary Martin on 4/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
